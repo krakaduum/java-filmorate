9435864d70f472c15c4d39be80fb423b47df7500
darina <krakaduum@yandex.ru>
2023-06-11 17:11:13 +0500

    Исправления БД

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,34 @@
-INSERT INTO PUBLIC.MPA (ID,NAME)
-	VALUES (1,'G');
-INSERT INTO PUBLIC.MPA (ID,NAME)
-	VALUES (2,'PG');
-INSERT INTO PUBLIC.MPA (ID,NAME)
-	VALUES (3,'PG-13');
-INSERT INTO PUBLIC.MPA (ID,NAME)
-	VALUES (4,'R');
-INSERT INTO PUBLIC.MPA (ID,NAME)
-	VALUES (5,'NC-17');
+INSERT INTO MPA (ID,NAME)
+	select 1, 'G' from dual
+	where not exists(select * from MPA where id = 1);
+INSERT INTO MPA (ID,NAME)
+	select 2,'PG' from dual
+	where not exists(select * from MPA where id = 2);
+INSERT INTO MPA (ID,NAME)
+	select 3,'PG-13' from dual
+	where not exists(select * from MPA where id = 3);
+INSERT INTO MPA (ID,NAME)
+	select 4,'R' from dual
+	where not exists(select * from MPA where id = 4);
+INSERT INTO MPA (ID,NAME)
+	select 5,'NC-17' from dual
+	where not exists(select * from MPA where id = 5);
 
-INSERT INTO PUBLIC.GENRE (ID,NAME)
-    VALUES (1,'Комедия');
-INSERT INTO PUBLIC.GENRE (ID,NAME)
-    VALUES (2,'Драма');
-INSERT INTO PUBLIC.GENRE (ID,NAME)
-    VALUES (3,'Мультфильм');
-INSERT INTO PUBLIC.GENRE (ID,NAME)
-    VALUES (4,'Триллер');
-INSERT INTO PUBLIC.GENRE (ID,NAME)
-    VALUES (5,'Документальный');
-INSERT INTO PUBLIC.GENRE (ID,NAME)
-    VALUES (6,'Боевик');
+INSERT INTO GENRE (ID,NAME)
+    select 1,'Комедия' from dual
+	where not exists(select * from GENRE where id = 1);
+INSERT INTO GENRE (ID,NAME)
+    select 2,'Драма' from dual
+	where not exists(select * from GENRE where id = 2);
+INSERT INTO GENRE (ID,NAME)
+    select 3,'Мультфильм' from dual
+	where not exists(select * from GENRE where id = 3);
+INSERT INTO GENRE (ID,NAME)
+    select 4,'Триллер' from dual
+	where not exists(select * from GENRE where id = 4);
+INSERT INTO GENRE (ID,NAME)
+    select 5,'Документальный' from dual
+	where not exists(select * from GENRE where id = 5);
+INSERT INTO GENRE (ID,NAME)
+    select 6,'Боевик' from dual
+	where not exists(select * from GENRE where id = 6);
